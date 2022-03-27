c5cb3301ad88db663f9a674861c38c37d8e58ca3
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-27 15:53:04 -0700

    Started output display
    Added <iomanip> to use setw

--- src/diceRollHistogramCPP/diceRollHistogramCPP/main.cpp
+++ src/diceRollHistogramCPP/diceRollHistogramCPP/main.cpp
@@ -12,6 +12,8 @@
 #include <iostream>
 // Used for getting time
 #include <ctime>
+// Used for setw()
+#include <iomanip>
 // Used for omitting std in cin cout
 using namespace std;
 
@@ -22,11 +24,11 @@ int rollDice();
 // Global Constants
 const int TOTAL_ROLLS = 1000;
 const int SIDES_ON_DIE = 6;
-const int SUM_OF_ROLLS = 13;   // Sum of rolls +1 to get an index location of 12
+const int SUM_OF_ROLLS = 12;   // Sum of rolls +1 to get an index location of 12
 
 int main(int argc, const char * argv[]) {
     // Declare array a size of 13
-    int pointCount[SUM_OF_ROLLS];
+    int pointCount[SUM_OF_ROLLS + 1] = {};
     // Used for storing sum of each roll
     int sumOfRoll;
     
@@ -39,7 +41,13 @@ int main(int argc, const char * argv[]) {
     }   // End of loop
     
     // Display output
-    
+    cout << "Roll        Total" << endl;
+    // Use for loop to display the roll and the totals
+    // i is the index/roll, starting at the roll of 2
+    for (int i = 2; i <= SUM_OF_ROLLS; i++) {
+        // Print the roll and the totals
+        cout << setw(3) << i << setw(13) << pointCount[i] << endl;
+    }   // End of print loop
     return 0;
 }
 
