357c66be6e65991bcaf3f4076e1cb91e21d4899a
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-27 15:05:30 -0700

    Created roll()
    Created roll()  function
    Utilizes srand to generate random numbers 1-6 like a die.

--- src/diceRollHistogramCPP/diceRollHistogramCPP/main.cpp
+++ src/diceRollHistogramCPP/diceRollHistogramCPP/main.cpp
@@ -8,9 +8,29 @@
  */
 
 #include <iostream>
+#include <ctime>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
+
+// Start roll function
+int roll() {
+    // Has random been initialized?
+    static bool randomInitialized = false;
+    
+    // This is only done the first time function is called
+    if (!randomInitialized) {
+        // Seed the random number generator
+        srand((unsigned int)time(NULL));
+        // Don't do it again
+        randomInitialized = true;
+    }   // End of seeding random
+    return (rand()%6) + 1;
+}   // End of roll
+
+// Start rollDice function
