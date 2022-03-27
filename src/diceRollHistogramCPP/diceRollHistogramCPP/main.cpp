a905ab7036f1a8a03c425eae446aa46e6a08b194
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-27 14:36:57 -0700

    Updated header

---
+++ src/diceRollHistogramCPP/diceRollHistogramCPP/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * diceRollHistogramCPP Version#1.0
+ * Frank Bernal
+ * CIS 054 C/C++ Programming
+ * Input: None
+ * Output: Histogram of dice rolls
+ * 27 Mar 2022
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
